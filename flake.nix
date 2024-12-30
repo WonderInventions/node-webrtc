c7f6dc0c87eed8a5b0a8b0cc75a61be4afcd545e
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2024-12-30 15:01:21 -0500

    format flake.nix

--- flake.nix
+++ flake.nix
@@ -3,44 +3,56 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         lib = nixpkgs.lib;
       in
       if lib.strings.hasSuffix "darwin" system then
-      {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            cmake
-            clang
-            clang-tools
-            darwin.apple_sdk.frameworks.AVFoundation
-            darwin.apple_sdk.frameworks.AppKit
-            darwin.apple_sdk.frameworks.Foundation
-            ninja
-            nodejs_20
-            pkg-config
-            zlib
-          ];
-        };
-      }
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              cmake
+              clang
+              clang-tools
+              darwin.apple_sdk.frameworks.AVFoundation
+              darwin.apple_sdk.frameworks.AppKit
+              darwin.apple_sdk.frameworks.Foundation
+              ninja
+              nodejs_20
+              pkg-config
+              zlib
+            ];
+          };
+        }
       else
-      {
-        devShells.default = (pkgs.buildFHSEnv.override {
-          stdenv = pkgs.gcc10Stdenv;
-        } {
-          name = "node-webrtc";
-          targetPkgs = pkgs: (with pkgs; [
-            cmake
-            gcc10
-            ninja
-            nodejs_20
-            pkg-config
-            zlib
-          ]);
-        }).env;
-      }
+        {
+          devShells.default =
+            (pkgs.buildFHSEnv.override
+              {
+                stdenv = pkgs.gcc10Stdenv;
+              }
+              {
+                name = "node-webrtc";
+                targetPkgs =
+                  pkgs:
+                  (with pkgs; [
+                    cmake
+                    gcc10
+                    ninja
+                    nodejs_20
+                    pkg-config
+                    zlib
+                  ]);
+              }
+            ).env;
+        }
     );
 }
