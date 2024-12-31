3cbf65e0f824dacd1bce52c1cabbd3a55c871917
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2024-12-30 19:07:39 -0500

    wip: get closer to compiling libwebrtc with nix clang

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
@@ -17,20 +17,39 @@
       in
       if lib.strings.hasSuffix "darwin" system then
         {
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              cmake
-              clang
-              clang-tools
-              darwin.apple_sdk.frameworks.AVFoundation
-              darwin.apple_sdk.frameworks.AppKit
-              darwin.apple_sdk.frameworks.Foundation
-              ninja
-              nodejs_20
-              pkg-config
-              zlib
-            ];
-          };
+          devShells.default =
+            let
+              apple-sdk = pkgs.apple-sdk;
+              clang = pkgs.llvmPackages_14.clang;
+              clang-tools = pkgs.llvmPackages_14.clang-tools;
+            in
+            pkgs.mkShell {
+              nativeBuildInputs =
+                (with pkgs; [
+                  cmake
+                  darwin.apple_sdk.frameworks.AVFoundation
+                  darwin.apple_sdk.frameworks.AppKit
+                  darwin.apple_sdk.frameworks.Foundation
+                  ninja
+                  nodejs_20
+                  pkg-config
+                  zlib
+                ])
+                ++ [
+                  clang
+                  clang-tools
+                ];
+              # Build variables based on documentation from https://github.com/timniederhausen/gn-build/blob/01c96fd9981b111a3a028356284968acd77fa435/README.md
+              shellHook = ''
+                cat <<EOF > nix.gni
+                is_clang=true
+                use_lld=false
+                clang_base_path="${clang}"
+                clang_use_chrome_plugins=false
+                mac_sdk_path="${apple-sdk}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
+                EOF
+              '';
+            };
         }
       else
         {
--- scripts/enter_webrtc.sh
+++ scripts/enter_webrtc.sh
@@ -3,9 +3,8 @@
 # A simple utility script for getting in the same context as configure-webrtc.sh, but interactively
 
 scripts="$(dirname -- "${BASH_SOURCE[0]}")"
-cd "${scripts}"
-build_folder="$(node -e "console.log(require('./build-vars.js').buildFolder)")"
-external="$(realpath "../${build_folder}/external")"
+build_folder="$(cd "${scripts}" && node -e "console.log(require('./build-vars.js').buildFolder)")"
+external="$(realpath "${build_folder}/external")"
 DEPOT_TOOLS="${external}/depot_tools/src"
 LIBWEBRTC="${external}/libwebrtc/download/src"
 
