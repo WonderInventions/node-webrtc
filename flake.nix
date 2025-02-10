74aee71b3f18982deb10b4ea9c5895637d94e91c
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2025-02-10 11:45:02 -0500

    make nix.gni on non-darwin too

--- flake.nix
+++ flake.nix
@@ -14,62 +14,50 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         lib = nixpkgs.lib;
+        is-darwin = lib.strings.hasSuffix "darwin" system;
       in
-      if lib.strings.hasSuffix "darwin" system then
-        {
-          devShells.default =
-            let
-              apple-sdk = pkgs.apple-sdk_12;
-              clang = pkgs.llvmPackages_14.clang;
-              clang-tools = pkgs.llvmPackages_14.clang-tools;
-            in
-            pkgs.mkShell {
-              nativeBuildInputs =
-                (with pkgs; [
-                  cmake
-                  ninja
-                  nodejs_20
-                  pkg-config
-                  zlib
-                ])
-                ++ [
-                  apple-sdk
-                  clang
-                  clang-tools
-                ];
-              # Build variables based on documentation from https://github.com/timniederhausen/gn-build/blob/01c96fd9981b111a3a028356284968acd77fa435/README.md
-              shellHook = ''
+      {
+        devShells.default =
+          let
+            apple-sdk = if is-darwin then pkgs.apple-sdk_12 else null;
+            clang = pkgs.llvmPackages_14.clang;
+            clang-tools = pkgs.llvmPackages_14.clang-tools;
+          in
+          pkgs.mkShell {
+            nativeBuildInputs =
+              (with pkgs; [
+                cmake
+                ninja
+                nodejs_20
+                pkg-config
+                zlib
+              ])
+              ++ [
+                clang
+                clang-tools
+              ]
+              ++ (if is-darwin then [ apple-sdk ] else [ ]);
+            # Build variables based on documentation from https://github.com/timniederhausen/gn-build/blob/01c96fd9981b111a3a028356284968acd77fa435/README.md
+            shellHook =
+              ''
                 cat <<EOF > nix.gni
                 is_clang=true
                 use_lld=false
                 clang_base_path="${clang}"
                 clang_use_chrome_plugins=false
-                mac_sdk_path="${apple-sdk}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
+              ''
+              + (
+                if is-darwin then
+                  ''
+                    mac_sdk_path="${apple-sdk}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
+                  ''
+                else
+                  ""
+              )
+              + ''
                 EOF
               '';
-            };
-        }
-      else
-        {
-          devShells.default =
-            (pkgs.buildFHSEnv.override
-              {
-                stdenv = pkgs.gcc10Stdenv;
-              }
-              {
-                name = "node-webrtc";
-                targetPkgs =
-                  pkgs:
-                  (with pkgs; [
-                    cmake
-                    gcc10
-                    ninja
-                    nodejs_20
-                    pkg-config
-                    zlib
-                  ]);
-              }
-            ).env;
-        }
+          };
+      }
     );
 }
