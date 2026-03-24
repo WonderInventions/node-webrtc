82b13357847f9777dc3f935d8699f949af6d1f70
Kevin Hanna <github@kdh.codes>
2026-03-24 13:41:45 +0000

    fix: expose NetworkThread() accessor on PeerConnectionFactory
    WebRTC's SctpTransport is constructed on the network thread and asserts
    owner_thread_->IsCurrent() on most methods (see the RTC_DCHECK_RUN_ON
    in the WebRTC source at pc/sctp_transport.cc lines 59, 66, 105). The
    node-webrtc code was dispatching to WorkerThread instead, which is the
    wrong thread. Expose NetworkThread so callers can dispatch correctly.

--- src/interfaces/rtc_peer_connection/peer_connection_factory.hh
+++ src/interfaces/rtc_peer_connection/peer_connection_factory.hh
@@ -47,6 +47,8 @@ public:
     return _factory;
   }
 
+  std::unique_ptr<rtc::Thread> &NetworkThread() { return _networkThread; }
+
   std::unique_ptr<rtc::Thread> &SignalingThread() { return _signalingThread; }
 
   std::unique_ptr<rtc::Thread> &WorkerThread() { return _workerThread; }
